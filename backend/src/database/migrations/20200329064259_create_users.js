7142f6730eeb30deb4ad0d1c24b24bc73edca3d0
Lawrence Melo <lawrencefmm@gmail.com>
2020-03-29 15:16:06 -0300

    change database tables

--- backend/src/database/migrations/20200329064259_create_users.js
+++ backend/src/database/migrations/20200329064259_create_users.js
@@ -5,16 +5,14 @@ exports.up = function(knex) {
         table.string('username').notNullable();
         table.string('name').notNullable();
         table.string('email').notNullable();
-        table.string('password').notNullable(); //Hash
-        table.string('pronouns').defaultTo('x'); //Should we default to non-binary in case of not specified? Will we ever use pronouns in future emails?
+        table.string('password').notNullable(); // Hash
         table.string('country').defaultTo('Brasil'); //Possibility for future internationalization. For now, defaulted to Brazil.
-        table.string('state').notNullable();
-        table.string('city').notNullable();
-        table.string('school_org'); //It is necessary? If so, nullable or notNullable?
-        table.string('birthday'); //It is necessary? If so, nullable or notNullable?
+        table.string('state').nullable();
+        table.string('city').nullable();
+        table.string('school_org').nullable(); //It is necessary? If so, nullable or notNullable?
+        table.string('birthday').nullable(); //It is necessary? If so, nullable or notNullable?
         table.string('permissions').defaultTo('user');
-        table.boolean('NOIC').defaultTo(0); //NOIC Collaborators will have special privilleges, their problems would have a tag on them and such. 
-        table.boolean('contact'); //Permission checkbox to contact their email;
+        table.boolean('contact'); // Permission checkbox to contact their email;
         table.timestamps();
         
       });
--- backend/src/database/migrations/20200329074927_create_problems.js
+++ backend/src/database/migrations/20200329074927_create_problems.js
@@ -2,16 +2,11 @@
 exports.up = function(knex) {
     return knex.schema.createTable('problems', function (table) {
         table.increments();
-        table.string('user_id').notNullable(); //Mod that created the problem.
-        table.string('test_cases').notNullable();
-        table.string('time_limit').notNullable();
-        table.string('runtime').notNullable();
-        table.string('challenge_rating').notNullable();
+        table.string('user_id').notNullable(); // Mod that created the problem.
         table.boolean('from_NOIC').notNullable();
-        table.timestamps(); 
-        
         table.foreign('user_id').references('id').inTable('users');
         table.foreign('from_NOIC').references('NOIC').inTable('users');
+        table.timestamps(); 
       });
 };
 
