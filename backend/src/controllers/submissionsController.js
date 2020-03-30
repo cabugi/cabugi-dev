d6b587ed53291b5eb4e72316bc5f96b2b5d79f29
ArthurAMP <arthurmpapp@gmail.com>
2020-03-30 17:47:57 -0300

    added submissionsController and created the delete submission and list submissions route

---
+++ backend/src/controllers/submissionsController.js
@@ -0,0 +1,38 @@
+const connection = require('../database/connection');
+
+module.exports = {
+    
+    // List users
+    async list(req, res) {
+
+        const { page = 1 } = req.query;
+        // List 10 users every page
+        const check = await connection('submissions')
+            .limit(10)
+            .offset((page - 1) * 20)
+            .select('*');
+
+        return res.json(check);
+    },
+
+    // Delete submissions
+    async delete(req, res) {
+
+        const {
+            id,
+            permissions // Mudar depois para recuperar esse valor pela database
+        } = req.headers;
+
+        if(permissions !== 'admin'){
+            return res.status(401).send("You cannot delete this submission.");
+        }
+        check = await connection.from('submissions').where('id', id);
+        if(check == ''){
+            return res.status(400).send("There is not a submission with the id \"" + id +"\"");
+        }
+
+        await connection.from('submissions').where('id', id).delete();
+        return res.status(200).send("Submission deleted.");
+    }
+}
+
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,14 @@
 const routes = require("express").Router();
 const usersController = require('./controllers/usersController');
 const sessionController = require('./controllers/sessionController');
+const submissionsController = require('./controllers/submissionsController')
 
 routes.get('/users', usersController.list);
 routes.post('/users/register', usersController.register);
 routes.delete('/users/delete', usersController.delete);
-
 routes.get('/users/login', sessionController.check);
+
+routes.get('/submissions', submissionsController.list);
+routes.delete('/submissions/delete', submissionsController.delete);
 
 module.exports = routes;
