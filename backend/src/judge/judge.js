d12c097b7ce7e8aa1efe13013e48dc29b1adfd12
Thiago Mota Martins <thiagomota510@gmail.com>
2020-03-30 17:20:00 -0300

    added judge

---
+++ backend/src/judge/judge.js
@@ -0,0 +1,52 @@
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec);
+
+const judge = async function(problemName, filePath, configPath) {
+    var {stdout, stderr} = await exec(`echo "submit -tl 1 -ml 256000 ${problemName} CPP17 \"${filePath}\"" | dmoj-cli -c "${configPath}"`);
+
+    var judge_response = [];
+
+    // Compilation error
+    if ( stdout.search("Failed compiling submission!") !== -1 ) {
+        let qtd = 0;
+
+        // Split in a list
+        stdout = stdout.split('\n');
+        stdout = stdout.slice(6, stdout.length - 4);
+        stdout = stdout.join('\n');
+
+        judge_response.push({
+            "status_code": "CE",
+            "error_log": stdout
+        });
+        
+    } else {
+        // Remove formatting
+        stdout = stdout.replace(/\u001b\[\d+m/gi, '');
+        
+        // Split in a list
+        stdout = stdout.split('\n');
+        stdout = stdout.slice(6, stdout.length - 4);
+
+        let batch = 0, test_case = 0;
+        for (let i = 0; i < stdout.length; i++) {     
+            if (stdout[i].search('Batch') !== -1) batch++;
+            else {
+                test_case++;
+                si_split = stdout[i].split(' ').filter( item => (item.length > 0) );
+                judge_response.push({
+                    batch,
+                    test_case,
+                    "status_code": si_split[3],
+                    "execution_time": ( si_split[4] === undefined ? undefined : si_split[4].slice(1) ), // remove first character
+                    "execution_memory": ( si_split[7] === undefined ? undefined : si_split[7].slice(0, -1) ) // remove last character
+                });
+                //console.log(si_split[4]);
+            }
+        }
+        //console.log(stdout);
+    }
+    return judge_response;
+}
+
+module.exports = judge;
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,8 @@
 const routes = require("express").Router();
 const connection = require('./database/connection');
 const crypto = require('crypto');
-
-
+const judge = require('./judge/judge')
+const fs = require('fs')
 
 routes.get('/users', async(req, res) => {
     const { username } = req.body;
@@ -10,6 +10,25 @@ routes.get('/users', async(req, res) => {
     const check = await connection.select('*').from('users').where('username', username);
     return res.json({check});
 });
+
+routes.post('/submit', async(req, res) => {
+    const body = req.body;
+
+    if (typeof body["source"] == "undefined") {
+        res.status(400).send("Invalid source code");
+    }else if (typeof body["problem"] == "undefined") {
+        res.status(400).send("Problem not found");
+    }else {
+
+        // Generate a random submisssion id
+        const id = crypto.randomBytes(4).toString('HEX');
+        fs.writeFile(`./src/judge/submissions/submission_${id}.cpp` ,body["source"], function (err) {
+            if (err) throw err;
+        });
+
+        res.status(200).send(await judge(body["problem"], `./src/judge/submissions/submission_${id}.cpp`, "./src/judge/config/config.yml"));
+     }
+})
 
 routes.post('/users/register', async (req, res)  => {
 
@@ -30,7 +49,7 @@ routes.post('/users/register', async (req, res)  => {
         // Check if a user with that username or email exists
         const checkUsername = await connection.select('*').from('users').where('username', username);
         const checkEmail = await connection.select('*').from('users').where('email', email);       
-        if(checkUsername.length || checkEmail.length) return res.status(400).send("User already exists");
+        if (checkUsername.length || checkEmail.length) return res.status(400).send("User already exists");
 
         // Generate a random id to user
         const id = crypto.randomBytes(4).toString('HEX');
