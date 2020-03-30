1ee1526c52e7e95c66c42cffc05b33c96062423e
Lawrence Melo <lawrencefmm@gmail.com>
2020-03-30 16:50:55 -0300

    fix a security issue

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -1,17 +1,18 @@
 const connection = require('../database/connection');
 
 module.exports = {
-	
+
 	async check(req, res) {
 
-		const { username } = req.body;
+		const { username, password} = req.body;
 
 		const checkUser = await connection('users').where({
 				username: `${username}`,
-			}).select('username').first();
+				password: `${password}`
+			}).select('username', 'password').first();
 
 		if(!checkUser) {
-			return res.status(404).send("User with that username not found");
+			return res.status(404).send("Wrong username or password");
 		}
 		return res.status(200).send("User found");
 	}
