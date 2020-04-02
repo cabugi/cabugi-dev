546cce53d87befeff8ec0a24f8d3faeb6938fcd4
ArthurAMP <arthurmpapp@gmail.com>
2020-04-02 02:11:10 -0300

    added new routes in routes.js

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,7 @@ const routes = require("express").Router();
 const usersController = require('./controllers/usersController');
 const sessionController = require('./controllers/sessionController');
 const submissionsController = require('./controllers/submissionsController')
+const problemsController = require('./controllers/problemsController')
 
 routes.get('/users', usersController.list);
 routes.get('/users/login', sessionController.check);
@@ -11,5 +12,9 @@ routes.delete('/users', usersController.delete);
 routes.get('/submissions', submissionsController.list);
 routes.post('/submissions', submissionsController.send);
 routes.delete('/submissions', submissionsController.delete);
+
+
+routes.post('/problems', problemsController.create);
+routes.get('/problems', problemsController.list);
 
 module.exports = routes;
