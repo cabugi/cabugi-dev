d8d2c7037b22aa9035244e6f98a12a86ca6b5923
ArthurAMP <arthurmpapp@gmail.com>
2020-04-02 01:30:38 -0300

    added create problems route

--- backend/src/controllers/problemsController.js
+++ backend/src/controllers/problemsController.js
@@ -2,5 +2,17 @@ const connection = require('../database/connection');
 
 module.exports = {
 
-    
+    async create(req, res) {
+
+        const {
+            user_id,
+            title
+        } = req.body;
+
+        await connection('problems').insert({
+            user_id,
+            title
+        });
+        return res.status(201).send("Problem created.");
+    },
 }
