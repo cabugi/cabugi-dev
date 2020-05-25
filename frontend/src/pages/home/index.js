601ba976ea87421167123c335eafd1ea345ad75b
Lawrence Melo <lawrencefmm@gmail.com>
2020-05-25 09:47:47 -0300

    create react app

---
+++ frontend/src/pages/home/index.js
@@ -0,0 +1,13 @@
+import 'react' from React;
+import './styles.css';
+import '../navbar/Navbar';
+
+export default function Home() {
+    return (
+        <div className="Home">
+            <Navbar />
+
+
+        </div>
+    );
+}
