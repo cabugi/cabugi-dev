1607b3867057eb12a836f86a47ba1268ae78dc13
Lawrence Melo <lawrencefmm@gmail.com>
2020-05-26 00:50:52 -0300

    home page

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,13 +1,19 @@
-import 'react' from React;
+import React from 'react';
 import './styles.css';
-import '../navbar/Navbar';
 
 export default function Home() {
+
     return (
         <div className="Home">
-            <Navbar />
-
-
+            <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;1,700&display=swap" rel="stylesheet"/>             
+            <div className="background-image" ></div>
+            <div className="clean-block clean-hero">
+                <div className="text">
+                    <h2>Lorem ipsum dolor sit amet.</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis vitae leo.</p>
+                    <button className="button" type="button">Learn More</button>
+                    </div>
+            </div>
         </div>
     );
 }
