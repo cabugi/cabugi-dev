b544f0b98308d12a16a5332c018bf3c838db94ac
Lawrence Melo <lawrencefmm@gmail.com>
2020-06-17 23:57:22 -0300

    Change Homepage

---
+++ frontend/src/pages/Home/Lottie.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Lottie from 'react-lottie';
+import animationData from './drawkit-grape-animation-2-LOOP.json'
+
+export default class LottieControl extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { isStopped: false, isPaused: false };
+    }
+
+    render() {
+
+        const defaultOptions = {
+            loop: true,
+            autoplay: true,
+            animationData: animationData,
+            rendererSettings: {
+                preserveAspectRatio: 'xMidYMid slice'
+            }
+        };
+
+        return <div>
+            <Lottie options={defaultOptions}
+                height={700}
+                width={700}
+                isStopped={this.state.isStopped}
+                isPaused={this.state.isPaused} />
+        </div>
+    }
+}
--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import './styles.css';
+import backgroundImage from "./wfh_1.svg"
 import Navbar from '../resources/Navbar/index'
+import "./Lottie"
+import LottieControl from './Lottie';
 
 export default function Home() {
 
     return (
         <div className="Home">
             <Navbar />
-            <div className="background-image" ></div>
-            <div className="clean-block clean-hero">
-                <div className="text">
-                    <h2>Lorem ipsum dolor sit amet.</h2>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mattis vitae leo.</p>
-                    <button className="button" type="button">Learn More</button>
+            <div className="content">
+
+                <div className="clean-block">
+                    <div className="text">
+                        <h1>Bem-vindo à plataforma de programadores</h1>
+                        <p>Comece já a estudar programação competitiva no site ai mt bom cria conta por favor</p>
+                        <button className="button" type="button">Cadastre-se</button>
+                    </div>
+                </div>
+                <div className="animation">
+                    <LottieControl loop="true" autoplay="true" />
                 </div>
             </div>
         </div>
